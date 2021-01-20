b45e2e58b00e152c6a704638dc973a0b3e60c28d
Shubhankar thakur <53370662+saims0n@users.noreply.github.com>
2021-01-21 01:46:50 +0530

    Update Badchar_Creator_Python_Script.py

--- Badchar_Creator_Python_Script.py
+++ Badchar_Creator_Python_Script.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
-listRem = "\\x23\\x3c\\x83\\xba".split("\\x")
+listRem = "\\x23\\x3c\\x83".split("\\x")
 for x in range(1, 256):
     if "{:02x}".format(x) not in listRem:
         print("\\x" + "{:02x}".format(x), end='')
 print()
+
+#qouted hex are bad char like \x00 in this program change this as your requirement, If you leave it empty and run it this will print all the possible hex.
